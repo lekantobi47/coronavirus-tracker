7709836427602db6a5ad8168790a221e11a31735
Olamilekan Akintilebo <lekantobi47@gmail.com>
2020-11-21 20:17:55 +0100

    initialize repository

---
+++ src/test/java/com/hayohtee/coronavirustracker/CoronavirusTrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hayohtee.coronavirustracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoronavirusTrackerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
